2ad641e4a44e19c5ad084351f1dca867a846ed05
andreihar <95883512+andreihar@users.noreply.github.com>
2024-02-22 17:58:17 -0800

    Implemented Contributions page

--- src/pages/Contributions.tsx
+++ src/pages/Contributions.tsx
@@ -1,9 +1,125 @@
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Contributions() {
+
   return (
     <>
-      <h1>Hello Page</h1>
+      <Navbar />
+      <section className="team-section py-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            {/* 2 */}
+            <div className="col-12 col-md-6">
+              <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+                <div className="card-body p-4">
+                  <div className="card-text pt-1">
+                    <h5 className="member-name mb-1 text-center text-primary font-weight-bold">Name</h5>
+                    <div className="mb-3 text-center">Cool Title?</div>
+                    <div>Some text?</div>
+                  </div>
+                </div>
+                <div className="card-footer theme-bg-primary border-0 text-center">
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+                <div className="card-body p-4">
+                  <div className="card-text pt-1">
+                    <h5 className="member-name mb-1 text-center text-primary font-weight-bold">Name</h5>
+                    <div className="mb-3 text-center">Cool Title?</div>
+                    <div>Some text?</div>
+                  </div>
+                </div>
+                <div className="card-footer theme-bg-primary border-0 text-center">
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* 3 */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+                <div className="card-body p-4">
+                  <div className="card-text pt-1">
+                    <h5 className="member-name mb-1 text-center text-primary font-weight-bold">Name</h5>
+                    <div className="mb-3 text-center">Cool Title?</div>
+                    <div>Some text?</div>
+                  </div>
+                </div>
+                <div className="card-footer theme-bg-primary border-0 text-center">
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+                <div className="card-body p-4">
+                  <div className="card-text pt-1">
+                    <h5 className="member-name mb-1 text-center text-primary font-weight-bold">Name</h5>
+                    <div className="mb-3 text-center">Cool Title?</div>
+                    <div>Some text?</div>
+                  </div>
+                </div>
+                <div className="card-footer theme-bg-primary border-0 text-center">
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+                <div className="card-body p-4">
+                  <div className="card-text pt-1">
+                    <h5 className="member-name mb-1 text-center text-primary font-weight-bold">Name</h5>
+                    <div className="mb-3 text-center">Cool Title?</div>
+                    <div>Some text?</div>
+                  </div>
+                </div>
+                <div className="card-footer theme-bg-primary border-0 text-center">
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                  <div className="progress mb-2 position-relative fs-6">
+                    <div className="progress-bar d-flex align-items-center" role="progressbar" style={{width: '30%'}} aria-valuenow={30} aria-valuemin={0} aria-valuemax={100}></div>
+                    <div className="position-absolute w-100 h-100 d-flex align-items-center justify-content-center">Website - 30%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
   )
 }
