11b6786ed4d0b332a91d64f165df9b0d5e68240f
andreihar <95883512+andreihar@users.noreply.github.com>
2024-05-16 16:10:28 -0700

    Added animations to Premise

--- src/components/Region.tsx
+++ src/components/Region.tsx
@@ -22,7 +22,7 @@ const Region: React.FC<RegionProps> = ({ region, isOpen, onRequestClose }) => {
         <h5 className="modal-title" id="characterOverlayLabel"></h5>
         <button type="button" className="btn-close fs-3" onClick={onRequestClose}></button>
       </div>
-      <div className="border-frame modal-body position-absolute text-white py-2 px-1 pb-1 pl-3" style={{ bottom: '10px', left: '10px', maxWidth: '600px' }}>
+      <div className="border-frame modal-body position-absolute text-white py-2 px-1 pb-1 pl-3" style={{ bottom: '10px', left: '10px', right: '10px', maxWidth: '600px' }}>
         <div className="d-flex justify-content-between">
           <div className="p-4">
             <h1>{region.name}</h1>
--- src/pages/Premise.tsx
+++ src/pages/Premise.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Navbar from '../components/Navbar';
 import text from '../assets/text.json';
 import image1 from '../assets/premise/image1.jpg';
 import image2 from '../assets/premise/image2.jpg';
@@ -36,21 +38,32 @@ function Premise() {
   }, []);
 
   return (
-    <div style={{ height: `${events * 100}vh`, position: 'relative' }}>
-      <div className="slide" style={{ position: 'fixed', top: '0', height: '100vh', width: '100%', backgroundImage: `url(${eventData[activeIndex].picture})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-        <div className="border-frame modal-body position-absolute text-white py-2 px-1 pb-1 pl-3" style={{ bottom: '10px', left: '10px', maxWidth: '600px' }}>
-          <div className="d-flex justify-content-between">
-            <div className="p-4">
-              <h4>{eventData[activeIndex].date}</h4>
-              <h1>{eventData[activeIndex].event}</h1>
-              <div className="overflow-auto my-4" style={{ maxHeight: '220px' }}>
-                <p>{eventData[activeIndex].description}</p>
+    <>
+      <Navbar />
+      <div className="position-relative" style={{ height: `${events * 100}vh` }}>
+        <TransitionGroup>
+          <CSSTransition
+            key={activeIndex}
+            timeout={500}
+            classNames="fade"
+          >
+            <div className="slide position-fixed w-100 h-100 top-0" style={{ backgroundImage: `url(${eventData[activeIndex].picture})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <div className="border-frame modal-body position-absolute text-white py-2 px-1 pb-1 pl-3" style={{ bottom: '10px', left: '10px', right: '10px', maxWidth: '600px' }}>
+                <div className="d-flex justify-content-between">
+                  <div className="p-4">
+                    <h4>{eventData[activeIndex].date}</h4>
+                    <h1>{eventData[activeIndex].event}</h1>
+                    <div className="overflow-auto my-4" style={{ maxHeight: '220px' }}>
+                      <p>{eventData[activeIndex].description}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
-    </div>
+    </>
   );
 }
 
